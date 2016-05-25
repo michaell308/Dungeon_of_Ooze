e748ec190d48ad61a700639d822b546a860135c8
michaell308 <michaell308@yahoo.com>
2016-05-25 00:50:16 -0500

    Light flicker added, game scene created

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour {
+    Light lightComp;
+    float minIntensity = 3.0f;
+    float maxIntensity = 8.0f;
+    float offset = 6; //amount of flicker
+	// Use this for initialization
+	void Start () {
+        lightComp = gameObject.GetComponent<Light>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float newIntensity = Mathf.Clamp(Random.Range(lightComp.intensity - offset, lightComp.intensity + offset),minIntensity, maxIntensity);
+        lightComp.intensity = Mathf.Lerp(lightComp.intensity, newIntensity, Time.deltaTime*2);
+    }
+}
